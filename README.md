f342cdd3969aedcdc0736ce7dca84e4ea38d7511
Iker <iker.sanchez@vizzuality.com>
2020-10-19 11:53:05 +0200

    first commit of basic package skeleton

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # vizzTools
+
+vizzTools is an internal Python library to manage some of our the day-to-day tasks.
+
+Currently this is a skeleton under development.
+
---
+++ vizzTools/__init__.py
@@ -0,0 +1,7 @@
+from .vizztools import Metadata
+#from pkg_resources import get_distribution
+
+#__version__ = get_distribution('PyPackage-template').version
+
+# Version of vizztools package
+__version__ = "0.0.1"
