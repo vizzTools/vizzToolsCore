d57a74a1ebfff9da10407463d5189137693ebadb
edwardpmorris <edward.morris@vizzuality.com>
2020-11-03 13:56:10 +0100

    added jsonld examples

--- docs/generate_schema_docs.py
+++ docs/generate_schema_docs.py
@@ -10,17 +10,30 @@ from json_schema_for_humans.generate import generate_from_filename
 
 # Create site directory structure
 schema_source_dir =  "/home/edward/Documents/vizzToolsCore/json-schema"
+jsonld_source_dir =  "/home/edward/Documents/vizzToolsCore/jsonld-examples"
 schema_dir = os.path.join(os.getcwd(), "docs", "json-schema")
 print("schema_dir", schema_dir)
-os.makedirs(os.path.join(os.getcwd(), "docs", "_includes"), exist_ok=True)
 schema_includes_dir = os.path.join(os.getcwd(), "docs", "_includes", "json-schema")
 print("schema_includes_dir", schema_includes_dir)
 os.makedirs(schema_includes_dir, exist_ok=True)
+jsonld_includes_dir = os.path.join(os.getcwd(), "docs", "_includes", "jsonld")
+print("jsonld_includes_dir", jsonld_includes_dir)
+os.makedirs(jsonld_includes_dir, exist_ok=True)
 schema_html_dir = os.path.join(os.getcwd(), "docs", "json-schema")
 print("schema_html_dir", schema_html_dir)
 os.makedirs(schema_html_dir, exist_ok=True)
 
-# Convert to HTML
+# Add JSON-LD
+for case_name in os.listdir(jsonld_source_dir):
+    print(f"Processing {case_name}")
+    name, ext = os.path.splitext(case_name)
+    case_source = os.path.abspath(os.path.join(jsonld_source_dir, case_name))
+    if not os.path.isfile(case_source) or ext != ".jsonld":
+        continue
+    shutil.copyfile(case_source, os.path.join(schema_dir, case_name))
+    shutil.copyfile(case_source, os.path.join(jsonld_includes_dir, case_name))
+
+# Convert Schema to HTML
 for case_name in os.listdir(schema_source_dir):
     print(f"Processing {case_name}")
     name, ext = os.path.splitext(case_name)
@@ -31,6 +44,7 @@ for case_name in os.listdir(schema_source_dir):
 
     shutil.copyfile(case_source, os.path.join(schema_dir, case_name))
     shutil.copyfile(case_source, os.path.join(schema_includes_dir, case_name))
+    
 
     print(f"Generating example {name}")
 
