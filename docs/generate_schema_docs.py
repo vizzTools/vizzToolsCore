5e2a0526de13a0df15d5da7ccf05155edbcafb23
edwardpmorris <edward.morris@vizzuality.com>
2020-11-03 10:43:25 +0100

    update

--- docs/generate_schema_docs.py
+++ docs/generate_schema_docs.py
@@ -1,45 +1,38 @@
-"""Run this to populate the example cases from the code to Jekyll folder"""
+"""Run this to populate the json-schema html docs"""
 
 import os
 import shutil
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+#sys.path.insert(0, os.path.abspath(".."))
 
 from json_schema_for_humans.generate import generate_from_filename
 
-cases_source_dir = os.path.abspath(os.path.join(os.path.dirname(os.getcwd()), "json-schema"))
-includes_dir = os.path.join(os.getcwd(), "_includes")
-json_examples_dir = os.path.join(os.getcwd(), "_includes", "examples")
-os.makedirs(json_examples_dir, exist_ok=True)
-asserts_dir = os.path.join(os.getcwd(), "assets")
-results_example_dir = os.path.join(asserts_dir, "examples")
-os.makedirs(results_example_dir, exist_ok=True)
+# Create site directory structure
+schema_source_dir =  "/home/edward/Documents/vizzToolsCore/json-schema"
+schema_examples_dir = os.path.join(os.getcwd(), "docs", "_includes", "json-schema")
+print("schema_examples_dir", schema_examples_dir)
+os.makedirs(schema_examples_dir, exist_ok=True)
+schema_html_dir = os.path.join(os.getcwd(), "docs", "json-schema")
+print("schema_html_dir", schema_html_dir)
+os.makedirs(schema_html_dir, exist_ok=True)
 
-#readme = "README.md"
-#readme_location = os.path.abspath(os.path.join(os.path.dirname(os.getcwd()), readme))
-#shutil.copyfile(readme_location, os.path.join(includes_dir, readme))
-
-#config_schema = "config_schema.json"
-#config_schema_location = os.path.abspath(os.path.join(os.path.dirname(os.getcwd()), config_schema))
-#generate_from_filename(config_schema_location, os.path.join(asserts_dir, "config_schema.html"), expand_buttons=True)
-
-
-for case_name in os.listdir(cases_source_dir):
+# Convert to HTML
+for case_name in os.listdir(schema_source_dir):
     print(f"Processing {case_name}")
     name, ext = os.path.splitext(case_name)
     name, _ = os.path.splitext(name)
-    case_source = os.path.abspath(os.path.join(cases_source_dir, case_name))
+    case_source = os.path.abspath(os.path.join(schema_source_dir, case_name))
     if not os.path.isfile(case_source) or ext != ".json":
         continue
 
-    shutil.copyfile(case_source, os.path.join(json_examples_dir, case_name))
+    shutil.copyfile(case_source, os.path.join(schema_examples_dir, case_name))
 
     print(f"Generating example {name}")
 
     generate_from_filename(
         case_source,
-        os.path.join(results_example_dir, f"{name}.html"),
+        os.path.join(schema_html_dir, f"{name}.html"),
         deprecated_from_description=True,
         expand_buttons=True,
     )
--- docs/index.md
+++ docs/index.md
@@ -2,9 +2,8 @@
 title: vizzToolsCore JSON Schema
 ---
 
-<h2>{{ site.data.examples.docs_list_title }}</h2>
 <ul>
-   {% for item in site.data.examples.docs %}
-      <li><a href="{{ item.url }}">{{ item.title }}</a></li>
+   {% for item in site.data.examples %}
+      <li><a href="json-schema/{{ item.title }}.html">{{ item.title }}</a></li>
    {% endfor %}
 </ul>
